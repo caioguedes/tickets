e4eac423869062b4ae0bc951f7f204a341dd848e
Caio Guedes <caio.cesar.g.souza@gmail.com>
2018-09-11 11:42:59 -0300

    Fix not found testing route

--- test/tickets/core_test.clj
+++ test/tickets/core_test.clj
@@ -24,8 +24,11 @@
       (is (= expected response))))
 
   (testing "Not found"
-    (let [response (parse-body (app (mock/request :get "/api/v1/not-found")))]
-      (is (= 1 1))))
+    (let [response (parse-body (app (mock/request :get "/api/v1/not-found")))
+          expected {:status 404
+                    :headers default-headers
+                    :body {:message "Not Found"}}]
+      (is (= expected response))))
 
   (testing "List tickets"
     (let [response (parse-body (app (mock/request :get "/api/v1/tickets")))
